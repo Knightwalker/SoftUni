403fc1b6a861fad69f8643d6d003bd8388f15e45
Knightwalker <deadlinessx@gmail.com>
2019-05-27 23:49:54 +0300

    edit

--- 03_Programming_Advanced/01_JS_Core/01_JavaScript_Fundamentals/07_Strings_and_RegEx_Lab/02_Find-Ascii-Equivalent/solution.js
+++ 03_Programming_Advanced/01_JS_Core/01_JavaScript_Fundamentals/07_Strings_and_RegEx_Lab/02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+	let input = document.getElementById("text").value;
+	let result = document.getElementById("result");
+	document.querySelector("#exercise > form > input[type=button]").addEventListener("click", findAsciiEquivalent(input));
 
-  //TODO...
+	function findAsciiEquivalent(input) {
+	  let split = input.split(" ").filter(a => a !== "");
+	  let output = "";
+
+	  for (let element of split) {
+	    if (Number(element)) {
+	      output += String.fromCharCode(element);
+	    } else {
+	      let charToNum = [];
+
+	      for (let i = 0; i < element.length; i++) {
+	        charToNum.push(element[i].charCodeAt(0));
+	      }
+	      let p = document.createElement("p");
+	      p.innerHTML = charToNum.join(" ");
+	      result.appendChild(p);
+	    }
+	  }
+
+	    let p = document.createElement("p");
+	    p.innerHTML = output;
+	    result.appendChild(p);
+	}
 }
