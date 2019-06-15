ccb178e1ee4dbddc30c5d608f78e9cb9f5782bac
Knightwalker <deadlinessx@gmail.com>
2019-06-16 00:15:21 +0300

    edit

--- 03_Programming_Advanced/01_JS_Core/03_JavaScript_Advanced/03_DOM_Manipulations_Lab/05_Mouse_Gradient/app.js
+++ 03_Programming_Advanced/01_JS_Core/03_JavaScript_Advanced/03_DOM_Manipulations_Lab/05_Mouse_Gradient/app.js
@@ -1,3 +1,18 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    
+    let resElement = document.getElementById("result");
+    let grdElement = document.getElementById("gradient");
+
+    grdElement.addEventListener("mousemove", function(e) {
+        let rect = this.getBoundingClientRect();
+        let leftPos = rect.left;
+        let rightPos = rect.right;
+
+        let grdPercent = (e.offsetX) / (rightPos - leftPos);
+        grdPercent = Math.floor(grdPercent * 100);
+
+        //console.log(`${leftPos} ${e.offsetX} ${rightPos}`);
+        resElement.textContent = grdPercent + "%";
+    });
+   
 }
--- 03_Programming_Advanced/01_JS_Core/03_JavaScript_Advanced/03_DOM_Manipulations_Lab/06_Highlight_Active/app.js
+++ 03_Programming_Advanced/01_JS_Core/03_JavaScript_Advanced/03_DOM_Manipulations_Lab/06_Highlight_Active/app.js
@@ -1,3 +1,38 @@
 function focus() {
-    console.log('TODO:...');
+
+    let inputs = document.getElementsByTagName('input');
+    Array.from(inputs).forEach(i => i.addEventListener('focus', onFocus)); 
+    Array.from(inputs).forEach(i => i.addEventListener('blur', onBlur));
+    function onFocus(event) {
+        let input = event.target;
+        input.parentNode.setAttribute('class', 'focused');
+    }
+    
+    function onBlur(event) {
+        let input = event.target;
+        input.parentNode.removeAttribute('class');
+    }
+    
 }
+
+/* Non working for Judge solution
+function focus() {
+    
+    let inputFields = Array.from(document.getElementsByTagName("input"));
+    for (inputField in inputFields) {
+        inputFields[inputField].addEventListener("click", Highlight);
+    }
+  
+    function Highlight(e) {
+        // clear highlingting for all
+        for (inputField in inputFields) {
+          inputFields[inputField].parentElement.removeAttribute('class');
+        }
+
+        // highlight target
+        let targetParentEl = e.target.parentElement;
+        targetParentEl.setAttribute('class', 'focused');
+    }
+
+}
+*/
