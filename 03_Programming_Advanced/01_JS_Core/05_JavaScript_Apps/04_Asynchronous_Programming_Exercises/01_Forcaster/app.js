eefcf79aef78ff2182fdf11444800e7f184b2680
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-07-16 14:09:23 +0300

    edit

--- 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/04_Asynchronous_Programming_Exercises/01_Forcaster/app.js
+++ 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/04_Asynchronous_Programming_Exercises/01_Forcaster/app.js
@@ -1,5 +1,138 @@
-function attachEvents() {
-    console.log("TODO...");
-}
+let locationEl = document.getElementById("location");
+let submitEl = document.getElementById("submit");
+let currentEl = document.getElementById("current");
+let upcomingEl = document.getElementById("upcoming");
 
-attachEvents();
+submitEl.addEventListener("click", function(){
+    let location = locationEl.value;
+    locationEl.value = "";
+    let locationName = "";
+    let locationCode = "";
+    
+    fetch("https://judgetests.firebaseio.com/locations.json")
+    .then(response => {
+        if (response.status != 200) {
+            throw new Error(`Custom Error: ${response.status}`);
+        }
+        return response.json();
+    }).then(function(data) {
+        // console.log("Fetch 1 Data");
+        // console.log(data);
+        let found = false;
+        for(let i = 0; i < data.length; i++) {
+            let item = data[i];
+            if (item.name == location) {
+                locationName = item.name;
+                locationCode = item.code;
+                found = true;
+                break;
+            }
+        }
+
+        if (found) { 
+            document.getElementById("forecast").style.display = "block";
+            currentEl.innerHTML = '<div class="label">Current conditions</div>';
+            upcomingEl.innerHTML = '<div class="label">Three-day forecast</div>';
+            fetchForecast(); 
+            fetchUpcomming();
+        }
+        else {
+            document.getElementById("forecast").style.display = "block";
+            currentEl.innerHTML = '<div class="label">Current conditions</div>'
+                                + '<div class="forecasts">Error: Invalid Input!</div>'
+                                ;
+            upcomingEl.innerHTML = '<div class="label">Three-day forecast</div>';
+        }
+
+    });
+    
+    function fetchForecast() {
+        // console.log(locationName);
+        // console.log(locationCode);
+
+        fetch(`https://judgetests.firebaseio.com/forecast/today/${locationCode}.json`)
+        .then(response => {
+            if (response.status != 200) {
+                throw new Error(`Custom Error: ${response.status}`);
+            }
+            return response.json();
+        }).then(data => {
+            // console.log("Fetch 2 Data");
+            // console.log(data);
+            let forecastsEl = document.createElement("div");
+            forecastsEl.className = "forecasts";
+                let conditionSymbol = document.createElement("span");
+                conditionSymbol.className = "condition symbol";
+
+                    let currentConditionStr = data.forecast.condition.toLowerCase().split(" ").join("");
+                    if (currentConditionStr == "sunny") { conditionSymbol.innerHTML = "&#x2600;"; }
+                    else if (currentConditionStr == "partlysunny") { conditionSymbol.innerHTML = "&#x26C5;"; }
+                    else if (currentConditionStr == "overcast") { conditionSymbol.innerHTML = "&#x2601;"; }
+                    else if (currentConditionStr == "rain") { conditionSymbol.innerHTML = "&#x2614;"; }
+              
+                let condition = document.createElement("span");
+                condition.className = "condition";
+                    let forecastData1 = document.createElement("span");  
+                    forecastData1.className = "forecast-data"; 
+                    forecastData1.textContent = data.name;         
+                    let forecastData2 = document.createElement("span");            
+                    forecastData2.className = "forecast-data";  
+                    forecastData2.innerHTML = `${data.forecast.low}${"&#176;"}/${data.forecast.high}${"&#176;"}`; 
+                    let forecastData3 = document.createElement("span");
+                    forecastData3.className = "forecast-data";
+                    forecastData3.textContent = data.forecast.condition; 
+                condition.appendChild(forecastData1);
+                condition.appendChild(forecastData2);
+                condition.appendChild(forecastData3);
+            forecastsEl.appendChild(conditionSymbol);
+            forecastsEl.appendChild(condition);
+            currentEl.appendChild(forecastsEl); 
+        });
+    }
+
+    function fetchUpcomming() {
+        fetch(`https://judgetests.firebaseio.com/forecast/upcoming/${locationCode}.json`)
+        .then(response => {
+            if (response.status != 200) {
+                throw new Error(`Custom Error: ${response.status}`);
+            }
+            return response.json();
+        }).then(data => {
+            // console.log("Fetch 3 Data");
+            // console.log(data);
+
+            let forecastsEl = document.createElement("div");
+            forecastsEl.className = "forecasts-info";
+
+            for(let key in data["forecast"]) {
+                let item = data["forecast"][key];
+
+                let upcomingEl2 = document.createElement("span");
+                upcomingEl2.className = "upcoming";
+                    let conditionSymbol = document.createElement("span");
+                    conditionSymbol.className = "symbol";
+                        let currentConditionStr = item.condition.toLowerCase().split(" ").join("");
+                        if (currentConditionStr == "sunny") { conditionSymbol.innerHTML = "&#x2600;"; }
+                        else if (currentConditionStr == "partlysunny") { conditionSymbol.innerHTML = "&#x26C5;"; }
+                        else if (currentConditionStr == "overcast") { conditionSymbol.innerHTML = "&#x2601;"; }
+                        else if (currentConditionStr == "rain") { conditionSymbol.innerHTML = "&#x2614;"; }
+
+                    let forecastData1 = document.createElement("span");
+                    forecastData1.className = "forecast-data";  
+                    forecastData1.innerHTML = `${item.low}${"&#176;"}/${item.high}${"&#176;"}`; 
+
+                    let forecastData2 = document.createElement("span");
+                    forecastData2.className = "forecast-data";
+                    forecastData2.textContent = item.condition; 
+                upcomingEl2.appendChild(conditionSymbol);
+                upcomingEl2.appendChild(forecastData1);
+                upcomingEl2.appendChild(forecastData2);
+                forecastsEl.appendChild(upcomingEl2);
+            }
+
+            upcomingEl.appendChild(forecastsEl);
+
+        });
+    }
+
+});
--- 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/04_Asynchronous_Programming_Exercises/02_FisherGame/app.js
+++ 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/04_Asynchronous_Programming_Exercises/02_FisherGame/app.js
@@ -1,6 +1,224 @@
-function attachEvents() {
-    console.log('TODO...');
+let loadBtn = document.querySelector("button.load");
+let addBtn = document.querySelector("button.add");
+loadBtn.addEventListener("click", loadAllCatches);
+addBtn.addEventListener("click", createCatch);
+
+let catchesEl = document.getElementById("catches");
+catchesEl.style.width = "100%";
+
+function loadAllCatches() {
+    catchesEl.innerHTML = "";
+
+    fetch("https://fisher-game.firebaseio.com/catches.json")
+    .then(response => {
+        if (response.status > 400) {
+            throw new Error(`Something went wrong. Error: ${response.statusText}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        
+        for (let key in data) {
+            let item = data[key];
+            //console.log(key);
+            //console.log(item);
+
+            let divCatch = document.createElement("div");
+            divCatch.className = "catch";
+            divCatch.setAttribute("data-id", key);
+            divCatch.innerHTML = '<label>Angler</label>'
+                          + `<input type="text" class="angler" value="${item.angler}">`
+                          + '<hr>'
+                          + '<label>Weight</label>' 
+                          + `<input type="number" class="weight" value="${item.weight}">`
+                          + '<hr>'
+                          + '<label>Species</label>'
+                          + `<input type="text" class="species" value="${item.species}">`
+                          + '<hr>'
+                          + '<label>Location</label>'
+                          + `<input type="text" class="location" value="${item.location}">`
+                          + '<hr>'
+                          + '<label>Bait</label>'
+                          + `<input type="text" class="bait" value="${item.bait}">`
+                          + '<hr>'
+                          + '<label>Capture Time</label>'
+                          + `<input type="number" class="captureTime" value="${item.captureTime}">`
+                          + '<hr>'
+                          + '<button class="update" onclick="updateCatch()">Update</button>'
+                          + '<button class="delete" onclick="deleteCatch()">Delete</button>'
+                          ;
+            catchesEl.appendChild(divCatch);
+        }
+    });
+
+}
+function createCatch() {
+    let validData = true;
+    let anglerEl = document.querySelector("#addForm > input.angler");
+    let weightEl = document.querySelector("#addForm > input.weight");
+    let speciesEl = document.querySelector("#addForm > input.species");
+    let locationEl = document.querySelector("#addForm > input.location");
+    let baitEl = document.querySelector("#addForm > input.bait");
+    let timeEl = document.querySelector("#addForm > input.captureTime");
+    if (anglerEl.value == "" || anglerEl.value == "this cannot be empty!") {
+        anglerEl.value = "this cannot be empty!";
+        anglerEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        anglerEl.style.border = "1px solid #ccc";
+    }
+    if (weightEl.value == "" || weightEl.value == -1) {
+        weightEl.value = -1;
+        weightEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        weightEl.style.border = "1px solid #ccc";
+    }
+    if (speciesEl.value == "" || speciesEl.value == "this cannot be empty!") {
+        speciesEl.value = "this cannot be empty!";
+        speciesEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        speciesEl.style.border = "1px solid #ccc";
+    }
+    if (locationEl.value == "" || locationEl.value == "this cannot be empty!") {
+        locationEl.value = "this cannot be empty!";
+        locationEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        locationEl.style.border = "1px solid #ccc";
+    }
+    if (baitEl.value == "" || baitEl.value == "this cannot be empty!") {
+        baitEl.value = "this cannot be empty!";
+        baitEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        baitEl.style.border = "1px solid #ccc";
+    }
+    if (timeEl.value == "" || timeEl.value == -1) {
+        timeEl.value = -1;
+        timeEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        timeEl.style.border = "1px solid #ccc";
+    }
+
+    if (!validData) { return; }
+
+    let data = { 
+        angler: anglerEl.value, 
+        weight: weightEl.value, 
+        species: speciesEl.value, 
+        location: locationEl.value, 
+        bait: baitEl.value, 
+        captureTime: timeEl.value
+    };
+
+    fetch("https://fisher-game.firebaseio.com/catches.json", {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{ 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+    .then(response => {
+      console.log('Success:', JSON.stringify(response)); 
+      alert("Item was added ! Reload the view!");  
+    })
+    .catch(error => console.error('Error:', error));
 }
 
-attachEvents();
+function updateCatch() {
+    console.log(event.currentTarget.parentNode);
+    let id = event.currentTarget.parentNode.getAttribute("data-id");
+    
+    let validData = true;
+    let anglerEl = event.currentTarget.parentNode.querySelector("#catches > div > input.angler");
+    let weightEl = event.currentTarget.parentNode.querySelector("#catches > div > input.weight");
+    let speciesEl = event.currentTarget.parentNode.querySelector("#catches > div> input.species");
+    let locationEl = event.currentTarget.parentNode.querySelector("#catches > div > input.location");
+    let baitEl = event.currentTarget.parentNode.querySelector("#catches > div > input.bait");
+    let timeEl = event.currentTarget.parentNode.querySelector("#catches > div > input.captureTime");
+    if (anglerEl.value == "" || anglerEl.value == "this cannot be empty!") {
+        anglerEl.value = "this cannot be empty!";
+        anglerEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        anglerEl.style.border = "1px solid #ccc";
+    }
+    if (weightEl.value == "" || weightEl.value == -1) {
+        weightEl.value = -1;
+        weightEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        weightEl.style.border = "1px solid #ccc";
+    }
+    if (speciesEl.value == "" || speciesEl.value == "this cannot be empty!") {
+        speciesEl.value = "this cannot be empty!";
+        speciesEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        speciesEl.style.border = "1px solid #ccc";
+    }
+    if (locationEl.value == "" || locationEl.value == "this cannot be empty!") {
+        locationEl.value = "this cannot be empty!";
+        locationEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        locationEl.style.border = "1px solid #ccc";
+    }
+    if (baitEl.value == "" || baitEl.value == "this cannot be empty!") {
+        baitEl.value = "this cannot be empty!";
+        baitEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        baitEl.style.border = "1px solid #ccc";
+    }
+    if (timeEl.value == "" || timeEl.value == -1) {
+        timeEl.value = -1;
+        timeEl.style.border = "2px solid red";
+        validData = false;
+    } else {
+        timeEl.style.border = "1px solid #ccc";
+    }
 
+    if (!validData) { return; }
+
+    let data = { 
+        angler: anglerEl.value, 
+        weight: weightEl.value, 
+        species: speciesEl.value, 
+        location: locationEl.value, 
+        bait: baitEl.value, 
+        captureTime: timeEl.value
+    };
+
+    fetch("https://fisher-game.firebaseio.com/catches/id.json", {
+        method: 'PUT', 
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{ 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+    .then(response => {
+      console.log('Success:', JSON.stringify(response)); 
+      alert("Item was edited! Reload the view!");  
+    })
+    .catch(error => console.error('Error:', error));
+
+}
+function deleteCatch() {
+    let id = event.currentTarget.parentNode.getAttribute("data-id");
+    event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode);
+
+    var data = null;
+    var xhr = new XMLHttpRequest();    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        alert("Item was deleted!");  
+      }
+    });
+    
+    xhr.open("DELETE", `https://fisher-game.firebaseio.com/catches/${id}.json`);
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.send(data);
+}
