5365bbcd5cd738725b68c237778c5e5caabdcf02
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-07-22 21:24:40 +0300

    edit

--- 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/06_Remote_Databases_Exercise/01_Books/app.js
+++ 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/06_Remote_Databases_Exercise/01_Books/app.js
@@ -1,1 +1,221 @@
-console.log('My requests...')
+const kinveyUsername = "guest";
+const kinveyPassword = "guest";
+const appKey = "kid_BkhXN1mGS";
+const appSecret = "1cf5f904786c4199a5b77f36384729be";
+const baseUrl = `https://baas.kinvey.com/appdata/${appKey}/books`;
+
+let tableBody = document.getElementById("tableBody");
+
+let btnCreateBook = document.getElementById("btnCreateBook");
+btnCreateBook.addEventListener("click", fCreateBook);
+
+let btnLoadBooks = document.getElementById("btnLoadBooks");
+btnLoadBooks.addEventListener("click", fLoadBooks);
+
+let btnDoneEdit = document.getElementById("btnDoneEdit");
+btnDoneEdit.addEventListener("click", fDoneEdit);
+let btnCancelEdit = document.getElementById("btnCancelEdit");
+btnCancelEdit.addEventListener("click", fCancelEdit);
+
+// fCreateBook
+function fCreateBook() {
+  event.preventDefault();
+  let title_el = document.getElementById("title");
+  let author_el = document.getElementById("author");
+  let isbn_el = document.getElementById("isbn");
+
+  // validate data
+  if (title_el.value != "" && author_el.value != "" && isbn_el.value != "") {
+    var data = {
+      title: title_el.value,
+      author: author_el.value,
+      isbn: isbn_el.value,
+    } 
+    title_el.value = "";
+    author_el.value = "";
+    isbn_el.value = "";
+  } else {
+    alert("Invalid data");
+    return 0;
+  }
+
+  const headers = {
+    method:"POST",
+    body: JSON.stringify(data),
+    credentials: "include",
+    Authorization: "Basic " + btoa(`${kinveyUsername}:${kinveyPassword}`),
+    headers: {
+      "Content-type":"application/json"
+    } 
+  };
+
+  fetch(baseUrl, headers)
+  .then(response => {
+    if (response.status >= 400) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(function(){
+    fLoadBooks();
+  })
+  .catch(err => console.log(err));
+}
+// END fCreateBook
+
+// fLoadBooks
+function fLoadBooks() {
+  const headers = {
+    credentials: "include",
+    Authorization: "Kinvey" + localStorage.getItem("authToken"),
+  }
+
+  fetch(baseUrl, headers)
+  .then(response => {
+    if (response.status >= 400) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    tableBody.innerHTML = "";
+    let fragment = document.createDocumentFragment();
+    for(let key in data) {
+      let id = data[key]["_id"];
+      let author = data[key]["author"];
+      let title = data[key]["title"];
+      let isbn = data[key]["isbn"];
+
+      let tr = document.createElement("tr");
+      tr.innerHTML = `<td>${title}</td>`
+                   + `<td>${author}</td>`
+                   + `<td>${isbn}</td>`
+                   + `<td>`
+                   +    `<button onclick="fEdit()" value="${id}">Edit</button>`
+                   +    `<button onclick="fDelete()" value="${id}">Delete</button>`
+                   + `</td>`
+                   ;
+      fragment.appendChild(tr);
+    }
+    tableBody.appendChild(fragment);
+
+  })
+  .catch(err => console.log(err));
+
+}
+// END fLoadBooks
+
+function fEdit() {
+  let title_el = document.getElementById("title");
+  let author_el = document.getElementById("author");
+  let isbn_el = document.getElementById("isbn");
+ 
+  let id = event.currentTarget.value;
+  let elementsArr = event.currentTarget.parentElement.parentElement.getElementsByTagName("td");
+  title_el.value = elementsArr[0].textContent;
+  author_el.value = elementsArr[1].textContent;
+  isbn_el.value = elementsArr[2].textContent;
+
+  btnCreateBook.style.display = "none";
+  btnDoneEdit.style.display = "block";
+  btnCancelEdit.style.display = "block";
+  btnDoneEdit.value = id;
+  btnCancelEdit.value = id;
+}
+
+function fCancelEdit() {
+  event.preventDefault();
+  let title_el = document.getElementById("title");
+  let author_el = document.getElementById("author");
+  let isbn_el = document.getElementById("isbn");
+ 
+  title_el.value = "";
+  author_el.value = "";
+  isbn_el.value = "";
+
+  btnCreateBook.style.display = "block";
+  btnDoneEdit.style.display = "none";
+  btnCancelEdit.style.display = "none";
+  btnDoneEdit.value = "-1";
+  btnCancelEdit.value = "-1";
+}
+
+function fDoneEdit() {
+  event.preventDefault();
+  let id = event.currentTarget.value;
+  let title_el = document.getElementById("title");
+  let author_el = document.getElementById("author");
+  let isbn_el = document.getElementById("isbn");
+
+  // validate data
+  if (title_el.value != "" && author_el.value != "" && isbn_el.value != "") {
+    var data = {
+      title: title_el.value,
+      author: author_el.value,
+      isbn: isbn_el.value,
+    } 
+    title_el.value = "";
+    author_el.value = "";
+    isbn_el.value = "";
+  } else {
+    alert("Invalid data");
+    return 0;
+  }
+
+  const headers = {
+    method:"PUT",
+    body: JSON.stringify(data),
+    credentials: "include",
+    Authorization: "Basic " + btoa(`${kinveyUsername}:${kinveyPassword}`),
+    headers: {
+      "Content-type":"application/json"
+    } 
+  };
+
+  const editUrl = `${baseUrl}/${id}`;
+  fetch(editUrl, headers)
+  .then(response => {
+    if (response.status >= 400) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(function(){
+    fLoadBooks();
+  })
+  .catch(err => console.log(err));
+
+  btnCreateBook.style.display = "block";
+  btnDoneEdit.style.display = "none";
+  btnCancelEdit.style.display = "none";
+  btnDoneEdit.value = "-1";
+  btnCancelEdit.value = "-1";
+
+}
+
+function fDelete() {
+  let id = event.currentTarget.value;
+
+  const headers = {
+    method:"DELETE",
+    credentials: "include",
+    Authorization: "Kinvey" + localStorage.getItem("authToken"),
+    headers: {
+      "Content-type":"application/json"
+    } 
+  };
+
+  const deleteUrl = `${baseUrl}/${id}`;
+  fetch(deleteUrl, headers)
+  .then(response => {
+    if (response.status >= 400) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(function(){
+    fLoadBooks();
+  })
+  .catch(err => console.log(err));
+
+}
--- 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/06_Remote_Databases_Exercise/03_Venuemaster/app.js
+++ 03_Programming_Advanced/01_JS_Core/05_JavaScript_Apps/06_Remote_Databases_Exercise/03_Venuemaster/app.js
@@ -1,1 +1,126 @@
-console.log('TODO...');
+const baseUrl = "https://baas.kinvey.com/";
+const base64Auth = btoa("guest:pass");
+const auth = {
+    "Authorization": "Basic " + base64Auth,
+    "Content-type": "application/json",
+};
+
+const inputElement = document.getElementById('venueDate');
+const venuesDivElement = document.getElementById('venue-info');
+const listVenuesBtn = document.getElementById('getVenues');
+
+listVenuesBtn.addEventListener('click', listVenues);
+
+function listVenues() {
+    let url = baseUrl + `rpc/kid_BJ_Ke8hZg/custom/calendar?query=${inputElement.value}`;
+    let workingOnId = '';
+
+    fetch(url, {
+        method: 'POST',
+        headers: auth
+    })
+        .then(handler)
+        .then(data => {
+            data.forEach(id => getVenuesInfo(id))
+        });
+
+    venuesDivElement.addEventListener('click', ev => {
+        let currentButton = ev.target;
+        if (currentButton.value === "More info") {
+            let buttonParentDiv = currentButton.parentNode.parentNode;
+            buttonParentDiv.getElementsByClassName('venue-details')[0].style.display = "block";
+            workingOnId = buttonParentDiv.getAttribute('id');
+            let selectElement = buttonParentDiv.getElementsByClassName('quantity')[0];
+            let ticketsCount = selectElement.options[selectElement.selectedIndex].value;
+            let purchaseBtn = buttonParentDiv.getElementsByClassName('purchase')[0];
+            purchaseBtn.addEventListener('click', () => {
+                venuesDivElement.innerHTML = makePurchase(buttonParentDiv, ticketsCount);
+
+                document.querySelector('input[value="Confirm"]').addEventListener('click', () => {
+                    let postUrl = baseUrl + `rpc/kid_BJ_Ke8hZg/custom/purchase?venue=${workingOnId}&qty=${ticketsCount}`;
+
+                    fetch(postUrl, {
+                        method: 'POST',
+                        headers: auth
+                    })
+                        .then(handler)
+                        .then(data => {
+                            venuesDivElement.innerHTML = `
+                            <p>You may print this page as your ticket.</p>
+                            ${data.html}
+                            `;
+                        })
+                })
+            });
+        }
+    });
+}
+
+function getVenuesInfo(id) {
+    let getUrl = baseUrl + `appdata/kid_BJ_Ke8hZg/venues/${id}`;
+
+    fetch(getUrl, {
+        method: 'GET',
+        headers: auth
+    })
+        .then(handler)
+        .then(venue => {
+            let divToAppend = document.createElement('div');
+            divToAppend.innerHTML = generateDivToAppend(venue);
+            venuesDivElement.appendChild(divToAppend);
+        });
+}
+
+function makePurchase(parentDiv, ticketsCount) {
+    let name = parentDiv.getElementsByClassName('venue-name')[0].textContent;
+    let price = parentDiv.getElementsByClassName('venue-price')[0].textContent;
+    let totalPrice = parseInt(price) * +ticketsCount;
+    let confirmationDivText = `
+    <span class="head">Confirm purchase</span>
+    <div class="purchase-info">
+        <span>${name}</span>
+        <span>${ticketsCount} x ${price}</span>
+        <span>Total: ${totalPrice} lv</span>
+        <input type="button" value="Confirm">
+    </div>`;
+
+    return confirmationDivText;
+}
+
+function generateDivToAppend(venue) {
+    let innerText = `<div class="venue" id="${venue._id}">
+                <span class="venue-name"><input class="info" type="button" value="More info">${venue.name}</span>
+                <div class="venue-details" style="display: none;">
+                    <table>
+                        <tr>
+                            <th>Ticket Price</th>
+                            <th>Quantity</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <td class="venue-price">${venue.price} lv</td>
+                            <td><select class="quantity">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select></td>
+                            <td><input class="purchase" type="button" value="Purchase"></td>
+                        </tr>
+                    </table>
+                    <span class="head">Venue description:</span>
+                    <p class="description">${venue.description}</p>
+                    <p class="description">Starting time: ${venue.startingHour}</p>
+                </div>
+            </div>`;
+
+    return innerText;
+}
+
+function handler(response) {
+    if (response.status >= 400) {
+        throw new Error(`Something went wrong. Error: ${response.statusText}`)
+    }
+    return response.json();
+}
