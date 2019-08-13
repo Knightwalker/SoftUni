eed8f7b9dc10cff91d9d99de21d9765322ba20f1
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-08-13 23:42:49 +0300

    edit

---
+++ cyber-bible.com/core/csi.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+	let elements = document.getElementsByTagName('*'), i;
+	console.log(elements);
+
+	for (i in elements) {
+		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
+			fragment(elements[i], elements[i].getAttribute('data-include'));
+		}
+	}
+
+	function fragment(el, url) {
+		let localTest = /^(?:file):/,
+			xmlhttp = new XMLHttpRequest(),
+			status = 0;
+
+		xmlhttp.onreadystatechange = function() {
+			/* if we are on a local protocol, and we have response text, we'll assume	things were sucessful */
+			if (xmlhttp.readyState == 4) {
+				status = xmlhttp.status;
+			}
+			if (localTest.test(location.href) && xmlhttp.responseText) {
+				status = 200;
+			}
+			if (xmlhttp.readyState == 4 && status == 200) {
+				el.outerHTML = xmlhttp.responseText;
+			}
+		}
+
+		try { 
+			xmlhttp.open("GET", url, true);
+			xmlhttp.send();
+		} catch(err) {
+			/* todo catch error */
+		}
+	}
+
+}
