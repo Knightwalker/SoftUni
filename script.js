47ae9a9f1d0caa7e37326e1c7b0c0b7a6bb5b10f
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-08-23 11:38:48 +0300

    edit

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 	function Story_CharacterCreationTest2() {
-		let game_shead = "";
-		let game_stext = "";
-		let game_sbtns = "";
-
 		game_shead = 'The History Classroom';
 		game_stext = '<br>'
 				   + '<div id="story_charactercreationtest1" class="game_simg"></div>'
 		 		   + '<br>'
 				   + '<p>You place the exam sheet on your desk and make yourself comfortable, while slightly closing your eyes under the harsh morning sunlight, beaming through the shutter cracks. Squinting, you lower your head towards the desk, looking away from <i>her</i>. Your new teacher... She was just standing there, infront of the chalkboard, captivating everybody with her pressence. I mean sure, you can sure tell she\'s pretty, all other boys from your class are all over her too, and even though she\'s a teacher and this is supposed to be wrong, you were having a hard time getting your mind off her, as if you were somehow being <i>charmed</i>. You could probably spend your whole day just looking at her, but unfortunately you don\'t really have that luxury. You gotta work hard - score high on this test - or else with your current grades, you might drop out from school sooner rather than later and this is not an option you would like to consider. Who would take care of your little sister then? You need to graduate, get a good job and better support your family... or whats left of it anyways.</p>'
+		 		   + '<br>'
+				   + `<p><span class="player">"I need to gather myself and focus!"</span>, you force your mind to behave and concentrate on the test. Atleast history was the only subject who was always interesting to you since you were a child. You hated it at first, but your passion on it started because of Elena, as you knew how much she loved history and you were always struggling in striking a conversation with her. It was actually a good thing that she was not here currently, as your mind was distracted enough already. Maybe it's her fault that you were not paying enough attention in other classes, but could you blame her? She was so delightful, that she manages to sneak into your mind every now and then. I mean just look at you, your thoughts are all over the place! You need to focus. You grab your pen and narrow your eyes towards the exam sheet on your desk, taking a good look at the questions, while a small smirk shines upon your face. You finally managed to concentrate and started working. You ought to admit that you had a slight advantage here. History was after all a subject you never felt naked on and after what felt like an hour, you are finally ready.</p>`
+		 		   + '<br>'
+		 		   + '<p><span class="player">"Teacher..."</span>, You say quietly, too light a tone to actually do anything, so you raise your hand and call her, with the faintest hint of a smile on your lips, <span class="player">"I am done..."</span>. You sound satisfied. You are really proud of your work and for a first time you have this feeling that everything was going to be alright... or was it really?</p>'
+		 		   + '<br>'
+				   + '<div id="story_charactercreationtest2" class="game_simg"></div>'
+		 		   + '<br>'
+		 		   + `<p><span class="enemy1">"Good job {name}, seems like you put a lot of work in there!"</span>, a rather familiar, but disturbing voice complimented you in a seemingly friendly manner, however you already knew who <i>he</i> was and anxiety stroke you hard, as you swiftly turned backwards to confirm it. It was him, {name}!... but when did he sneak behind you. Was he planning to torture you again? Was tommorow's bullying not enough for him? Couldn't he just leave you alone for once!</p>`
+		 		   + '<br>'
+		 		   + `<p><span class="enemy1">"There's no need to get so tense {name}. In fact, I would like to make you a deal"</span>, he said with a confident, seemingly friendly disposition, but a determined posture of someone who knows exactly what they want. You could only narrow your eyes as you looked at him, but you knew him well enough to trust a single word comming out of his mouth. <span class="player">"What deal? What do you want?</span>, you ask him, unable to look him straight into the eyes. <span class="player">"If this is about the coffe, I already promised to fix my mistake and buy the proper one."</span>, you tell him, feeling your pulse quicken as you are barely able to talk back to him.</p>`
+				   + '<br>'
+				   + `<p><span class="enemy1">"Ooh arent you being a little too loud."</span>, he exclaimed, while his feet stayed leaned on your chair, as if he was ready to squash you into your desk, with one kick, given that you provide his ears something he does not want to hear. Your senses sharpen, your grip on the desk and the chair strengthens, as your body is preparing itself for anything, as much as it can, while awaiting for what he has to say. You detect a movement out of him, as he places his feet down, followed by a deep sigh. <span class="enemy1">"Listen {name}, I'm sorry... Let's be pals, huh?"</span>, he breathed out heavily and in that moment an awkward silence falls between you, as you finally looked him in the eyes and for the first time, you were completely unable to figure out what he wants. Frankly, you had no idea what just happened. What is this "Sorry"? There is no such word in his vocabulary. With this guy, there is only black and white. He wants something from you, you don't give it to him, he beats you... you give it to him, he doesn't beat you... There is no gray!</p>`
+				   + '<br>'	
+				   + '<div id="story_charactercreationtest3" class="game_simg"></div>'
+		 		   + '<br>'
+		 		   + `<p><span class="enemy1">"Hear me out {name}, I know that I have been mean with you, but do you think that you are the only one that have problems?"</span>, not so much a threat or a command, which was unusual comming out of him. What problems could he possibly have? You wonder why is he going so far, as to even speak with you in the first place, he's never done that. Which means that something must have happened to him. <span class="enemy1">"Take a good look at my face... How do you think somebody like me got those bruises, eh? I'm dealing with alot of shit... but i'll give him what <i>he</i> wants and it all ends tonight."</span>, he says and im finally able to look him straight. He was looking perfectly fine this morning when he beated you to a pulp, but now when you took a closer look at him you realised that he is just another form of a slave, like yourself. There is a chain of command here, he answers to somebody, but who could possibly beat this guy, what a monster could he possibly be? <span class="enemy1">"Now since everybody from my crew chickened out, because of those stupid animal attacks and the cops, I will make you the following offer. If you come with me to that place tonight and help me, I will never ever bother you again"</span>, he reassures you and it doesn't sound like he is lying, but this also doesn't mean that you can trust him either. One thing is certain thou, tonight is a full moon and this is no coincidence. Whoever is commanding this guy knows the lore well and it will be really bad if they get what you are searching for before you! You must make a choice!</p>`
+		 		   + '<br>'
+				   ;
+
+		game_sbtns = '<div id="game_choice">Do you want to:</div>'
+				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest3\')">Say that you will go with him.</div>'
+				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest3\')">Say that you must think about it</div>'
+				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest3\')">Do not say anything yet.</div>'
+				   ;	
+
+		document.getElementById('game_shead').innerHTML = game_shead;
+		document.getElementById('game_stext').innerHTML = game_stext; 
+		document.getElementById('game_sbtns').innerHTML = game_sbtns;	
+	}
+
+	function Story_CharacterCreationTest3() {
+		game_shead = 'The History Classroom';
+		game_stext = '<br>'
+				   + '<div id="story_charactercreationtest4" class="game_simg"></div>'
+		 		   + '<br>'
+				   + `<p>The teacher slowly approached, while the following words slipped out of your mouth, <span class="player">"Fine... ill come"</span>, you answer, thinking at how his words feel just as shady as everything else that this guy does. Tonight is a special night, you must be carefully as too many factors are coming into play already and you just told him that you will go with him. However, saying that you will go and actually going is a totally different thing, yet you can't imagine what he might do to you later if you just outstraight lied to him. You must make up your mind before tonight.</p>`
 		 		   + '<br>'
 				   + '<p><span class="player">"I need to gather myself and focus!"</span>, you force your mind to behave and concentrate on the test. Atleast history was the only subject who was always interesting to you since you were a child. You hated it at first, but your passion on it started because of Elena, as you knew how much she loved history and you were always struggling in striking a conversation with her. It was actually a good thing that she was not here currently, as your mind was distracted enough already. Maybe it\'s her fault that you were not paying enough attention in other classes, but could you blame her? She was so delightful, that she manages to sneak into your mind every now and then. I mean just look at you, your thoughts are all over the place! You need to focus. You grab your pen and narrow your eyes towards the exam sheet on your desk, taking a good look at the questions, while a small smirk shines upon your face. You finally managed to concentrate and started working. You ought to admit that you had a slight advantage here. History was after all a subject you never felt naked on and after what felt like an hour, you are finally ready.</p>'
 		 		   + '<br>'
@@ -29,12 +61,11 @@
 
 		game_sbtns = '<div id="game_choice">Do you want to:</div>'
 				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest\')">Say that you will go with him.</div>'
-				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest\')">Lie that you will go with him.</div>'
-				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest\')">Refuse to go with him.</div>'
+				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest\')">Lie that you must think about it</div>'
 				   + '<div class="choice_block noselect" onclick="OpenPlace(\'Place_1_2\', \'story\', \'story_charactercreationtest\')">Do not say anything.</div>'
 				   ;	
 
 		document.getElementById('game_shead').innerHTML = game_shead;
-		document.getElementById('game_stext').innerHTML = game_stext; 
+		document.getElementById('game_stext').innerHTML += game_stext; 
 		document.getElementById('game_sbtns').innerHTML = game_sbtns;	
 	}
