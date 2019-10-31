9e0ca52895c65f6d37ea382dcc93b8613a7ab018
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-10-31 15:52:48 +0200

    edit

---
+++ 03_Programming_Advanced/04_C#_OOP/01_OOP_WorkingWithAbstraction/01_WorkingWithAbstractionLab/01_RhombusOfStars/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace _01_RhombusOfStars
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawStar(n);
+        }
+
+        public static void DrawStar(int n)
+        {
+            StringBuilder figure = new StringBuilder();
+
+            // top
+            for (int i = 0; i < n - 1; i++)
+            {
+                figure.Append(new string(' ', n - 1 - i));
+                DrawLineOfStars(figure, i + 1);
+                figure.Append(new string(' ', n - 1 - i));
+                figure.AppendLine();
+            }
+
+            // bottom
+            for (int i = n - 1; i >= 0; i--)
+            {
+                figure.Append(new string(' ', n - 1 - i));
+                DrawLineOfStars(figure, i + 1);
+                figure.Append(new string(' ', n - 1 - i));
+                figure.AppendLine();
+            }
+
+            Console.WriteLine(figure);
+        }
+
+        public static void DrawLineOfStars(StringBuilder figure, int numberOfStars)
+        {
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                figure.Append("*");
+                if (i < numberOfStars - 1)
+                {
+                    figure.Append(" ");
+                }
+         
+            }
+        
+        }
+
+    }
+}
---
+++ 03_Programming_Advanced/04_C#_OOP/01_OOP_WorkingWithAbstraction/01_WorkingWithAbstractionLab/02_PointInRectangle/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _02_PointInRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rectangleCoordinates = Console.ReadLine().Split(" ");
+            int topLeftX = int.Parse(rectangleCoordinates[0]);
+            int topLeftY = int.Parse(rectangleCoordinates[1]);
+            int bottomRightX = int.Parse(rectangleCoordinates[2]);
+            int bottomRightY = int.Parse(rectangleCoordinates[3]);
+
+            Rectangle rect = new Rectangle(topLeftX, topLeftY, bottomRightX, bottomRightY);
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] pointCoordinates = Console.ReadLine().Split(" ");
+                int x = int.Parse(pointCoordinates[0]);
+                int y = int.Parse(pointCoordinates[1]);
+
+                Point point = new Point(x, y);
+                Console.WriteLine(rect.Contains(point));
+            }
+
+        }
+    }
+
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Rectangle
+    {
+
+        public Rectangle(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY)
+        {
+            this.topLeftX = topLeftX;
+            this.topLeftY = topLeftY;
+            this.bottomRightX = bottomRightX;
+            this.bottomRightY = bottomRightY;
+        }
+
+        public int topLeftX { get; set; }
+        public int topLeftY { get; set; }
+        public int bottomRightX { get; set; }
+        public int bottomRightY { get; set; }
+
+        public bool Contains(Point point)
+        {
+            if (point.X >= this.topLeftX && point.X <= this.bottomRightX)
+            {
+                if (point.Y >= this.topLeftY && point.Y <= this.bottomRightY)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+}
