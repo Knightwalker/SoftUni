0a94b4be993d633e67cc76928c6c1657fd09073a
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-11-01 15:59:56 +0200

    edit

---
+++ 03_Programming_Advanced/04_C#_OOP/01_OOP_WorkingWithAbstraction/01_WorkingWithAbstractionLab/04_HotelReservation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _04_HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var priceCalculator = new PriceCalculator(input);
+
+            Console.WriteLine(priceCalculator.GetTotalPrice().ToString("F2"));
+
+        }
+    }
+
+    public enum SeasonMultiplier
+    {
+        Autumn = 1,
+        Spring = 2,
+        Winter = 3,
+        Summer = 4
+    }
+
+    public enum DiscountPercentage
+    {
+        None,
+        SecondVisit = 10,
+        VIP = 20
+    }
+
+    public class PriceCalculator
+    {
+        private decimal pricePerNight;
+        private int nights;
+        private SeasonMultiplier seasonMultiplier;
+        private DiscountPercentage discountPercentage;
+
+        public PriceCalculator(string[] commandArgs)
+        {
+            pricePerNight = decimal.Parse(commandArgs[0]);
+            nights = int.Parse(commandArgs[1]);
+            seasonMultiplier = Enum.Parse<SeasonMultiplier>(commandArgs[2]);
+            discountPercentage = DiscountPercentage.None;
+
+            if (commandArgs.Length == 4)
+            {
+                discountPercentage = Enum.Parse<DiscountPercentage>(commandArgs[3]);
+            }
+        }
+
+        public decimal GetTotalPrice()
+        {
+            decimal basePrice = pricePerNight * nights * (int)seasonMultiplier;
+
+            return basePrice - basePrice * (decimal)discountPercentage / 100;
+        }
+    }
+
+}
