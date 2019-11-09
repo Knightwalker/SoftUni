6d051cab4e2f1e75003853250ea9d8e4d2569ac2
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-11-10 00:12:44 +0200

    edit

---
+++ 03_Programming_Advanced/04_C#_OOP/03_Encapsulation/EncapsulationExercise/03_ShoppingSpree/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            string[] input2 = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            foreach (var pair in input1)
+            {
+                var tokens = pair.Split('=');
+                string name = tokens[0];
+                decimal money = decimal.Parse(tokens[1]);
+
+                try
+                {
+                    persons.Add(new Person(name, money));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            foreach (var pair in input2)
+            {
+                var tokens = pair.Split('=');
+
+                try
+                {
+                    products.Add(new Product(tokens[0], decimal.Parse(tokens[1])));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END") { break; }
+
+                var tokens = command.Split();
+                string personName = tokens[0];
+                string productName = tokens[1];
+
+                var person = persons.Single(p => p.Name == personName);
+                var product = products.Single(p => p.Name == productName);
+
+                if (!person.BuyProduct(product))
+                {
+                    Console.WriteLine($"{person.Name} can't afford {product.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} bought {product.Name}");
+                }
+            }
+
+            foreach (var person in persons)
+            {
+                string productsBought = person.Products.Count == 0 ? "Nothing bought" : String.Join(", ", person.Products.Select(p => p.Name));
+
+                Console.WriteLine($"{person.Name} - {productsBought}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        private decimal Money
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products
+        {
+            get
+            {
+                return this.products;
+            }
+        }
+
+        public bool BuyProduct(Product product)
+        {
+            if (this.money >= product.Cost)
+            {
+                this.money -= product.Cost;
+                this.products.Add(product);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.cost = value;
+            }
+        }
+    }
+
+}
---
+++ 03_Programming_Advanced/04_C#_OOP/03_Encapsulation/EncapsulationExercise/04_PizzaCalories/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_PizzaCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var tokens = Console.ReadLine().Split();
+                Pizza pizza = new Pizza(tokens[1]);
+                tokens = Console.ReadLine().Split();
+                pizza.Dough = new Dough(tokens[1], tokens[2], int.Parse(tokens[3]));
+
+                string command;
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    tokens = command.Split();
+                    pizza.AddTopping(new Topping(tokens[1], int.Parse(tokens[2])));
+                }
+
+                Console.WriteLine(pizza);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    public class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+
+        public Pizza(string name)
+        {
+            this.Name = name;
+            this.toppings = new List<Topping>();
+        }
+
+        private string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value.Length < 1 || value.Length > 15)
+                {
+                    throw new ArgumentException("Pizza name should be between 1 and 15 symbols.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public Dough Dough
+        {
+            set
+            {
+                this.dough = value;
+            }
+        }
+
+        public void AddTopping(Topping topping)
+        {
+            if (this.toppings.Count == 10)
+            {
+                throw new ArgumentException("Number of toppings should be in range [0..10].");
+            }
+
+            this.toppings.Add(topping);
+        }
+
+        public double Calories()
+        {
+            double sum = 0;
+            sum += this.dough.Calories();
+            this.toppings.ForEach(t => sum += t.Calories());
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Calories():f2} Calories.";
+        }
+    }
+
+    public class Dough
+    {
+        private const int BaseCaloriesPerGram = 2;
+
+        private static object[] allowedFlourTypes = {
+            new { FlourType = "white", CaloriesModifier = 1.5 },
+            new { FlourType = "wholegrain", CaloriesModifier = 1.0 }
+        };
+
+        private static object[] allowedBakingTechniqueTypes = {
+            new { BakingTechnique = "crispy", CaloriesModifier = 0.9 },
+            new { BakingTechnique = "chewy", CaloriesModifier = 1.1 },
+            new { BakingTechnique = "homemade", CaloriesModifier = 1.0 }
+        };
+
+        private string flourType;
+        private string bakingTechnique;
+        private int weight;
+
+        public Dough(string flourType, string bakingTechnique, int weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+
+        private string FlourType
+        {
+            get
+            {
+                return this.flourType;
+            }
+
+            set
+            {
+                if (!allowedFlourTypes.Any(
+                    ft => (string)ft.GetType().GetProperty("FlourType").GetValue(ft, null) == value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                this.flourType = value;
+            }
+        }
+
+        private string BakingTechnique
+        {
+            get
+            {
+                return this.bakingTechnique;
+            }
+
+            set
+            {
+                if (!allowedBakingTechniqueTypes.Any(
+                    bt => (string)bt.GetType().GetProperty("BakingTechnique").GetValue(bt, null) == value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                this.bakingTechnique = value;
+            }
+        }
+
+        private int Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+
+            set
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new ArgumentException("Dough weight should be in the range [1..200].");
+                }
+
+                this.weight = value;
+            }
+        }
+
+        public double Calories()
+        {
+            var ftObject = allowedFlourTypes.First(ft => (string)ft.GetType().GetProperty("FlourType").GetValue(ft, null) == this.FlourType.ToLower());
+            var ftModifier = (double)ftObject.GetType().GetProperty("CaloriesModifier").GetValue(ftObject, null);
+
+            var btObject = allowedBakingTechniqueTypes.First(bt => (string)bt.GetType().GetProperty("BakingTechnique").GetValue(bt, null) == this.BakingTechnique.ToLower());
+            var btModifier = (double)btObject.GetType().GetProperty("CaloriesModifier").GetValue(btObject, null);
+
+            return BaseCaloriesPerGram * ftModifier * btModifier * this.Weight;
+        }
+    }
+
+    public class Topping
+    {
+        private const int BaseCaloriesPerGram = 2;
+
+        private static object[] allowedToppingTypes = {
+        new { ToppingType = "meat", CaloriesModifier = 1.2 },
+        new { ToppingType = "veggies", CaloriesModifier = 0.8 },
+        new { ToppingType = "cheese", CaloriesModifier = 1.1 },
+        new { ToppingType = "sauce", CaloriesModifier = 0.9 }
+    };
+
+        private string toppingType;
+        private int weight;
+
+        public Topping(string toppingType, int weight)
+        {
+            this.ToppingType = toppingType;
+            this.Weight = weight;
+        }
+
+        private string ToppingType
+        {
+            get
+            {
+                return this.toppingType;
+            }
+
+            set
+            {
+                if (!allowedToppingTypes.Any(
+                    tt => (string)tt.GetType().GetProperty("ToppingType").GetValue(tt, null) == value.ToLower()))
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+
+                this.toppingType = value;
+            }
+        }
+
+        private int Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+
+            set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new ArgumentException($"{this.ToppingType} weight should be in the range [1..50].");
+                }
+
+                this.weight = value;
+            }
+        }
+
+        public double Calories()
+        {
+            var ttObject = allowedToppingTypes.First(tt => (string)tt.GetType().GetProperty("ToppingType").GetValue(tt, null) == this.ToppingType.ToLower());
+            var ttModifier = (double)ttObject.GetType().GetProperty("CaloriesModifier").GetValue(ttObject, null);
+
+            return BaseCaloriesPerGram * ttModifier * this.Weight;
+        }
+    }
+
+}
