ae8eb0e403c86adf0bebc396f9d5897eb185f690
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-11-11 13:47:16 +0200

    edit

---
+++ 03_Programming_Advanced/04_C#_OOP/04_InterfacesAndAbstraction/InterfacesAndAbstractionLab/01_Shapes/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+            rect.Draw();
+        }
+
+        public interface IDrawable
+        {
+            void Draw();
+        }
+
+        public class Circle : IDrawable
+        {
+            private int radius;
+
+            public Circle(int radius)
+            {
+                this.Radius = radius;
+            }
+
+            public int Radius
+            {
+                get
+                {
+                    return this.radius;
+                }
+
+                private set
+                {
+                    this.radius = value;
+                }
+            }
+
+            public void Draw()
+            {
+                double rIn = this.Radius - 0.4;
+                double rOut = this.Radius + 0.4;
+
+                for (double y = this.Radius; y >= -this.Radius; --y)
+                {
+                    for (double x = -this.Radius; x < rOut; x += 0.5)
+                    {
+                        double value = x * x + y * y;
+                        if (value >= rIn * rIn && value <= rOut * rOut)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public class Rectangle : IDrawable
+        {
+            private int width;
+            private int height;
+
+            public Rectangle(int width, int height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+
+            public int Width
+            {
+                get
+                {
+                    return this.width;
+                }
+
+                private set
+                {
+                    this.width = value;
+                }
+            }
+            public int Height
+            {
+                get
+                {
+                    return this.height;
+                }
+
+                private set
+                {
+                    this.height = value;
+                }
+            }
+
+            public void Draw()
+            {
+                DrawLine(this.Width, '*', '*');
+                for (int i = 1; i < this.Height - 1; i++)
+                {
+                    DrawLine(this.Width, ' ', '*');
+                }
+                DrawLine(this.Width, '*', '*');
+            }
+
+            private void DrawLine(int width, char mid, char end)
+            {
+                Console.Write(end);
+                for (int i = 1; i < width - 1; i++)
+                {
+                    Console.Write(mid);
+                }
+                Console.WriteLine(end);
+            }
+        }
+
+    }
+}
---
+++ 03_Programming_Advanced/04_C#_OOP/04_InterfacesAndAbstraction/InterfacesAndAbstractionLab/02_Cars/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(tesla.ToString());
+        }
+    }
+
+    public interface IElectricCar
+    {
+        int Battery { get; }
+    }
+
+    public interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Start();
+        public string Stop();
+    }
+
+    public class Seat : ICar
+    {
+        private string name = "Seat";
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.name} {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+            return sb.ToString();
+        }
+    }
+
+    public class Tesla : ICar, IElectricCar
+    {
+        private string name = "Tesla";
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.Append(this.Stop());
+            return sb.ToString();
+        }
+    }
+
+}
