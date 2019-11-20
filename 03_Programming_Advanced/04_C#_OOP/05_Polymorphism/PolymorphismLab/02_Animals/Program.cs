139523a5611b7fc5aaa26b437f14eb5be2720422
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2019-11-20 16:58:34 +0200

    edit

---
+++ 03_Programming_Advanced/04_C#_OOP/05_Polymorphism/PolymorphismLab/02_Animals/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public class StartUp 
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat("Pesho", "Whiskas");
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+        }
+    }
+
+    public class Animal
+    {
+        protected Animal(string name, string favoriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favoriteFood;
+        }
+
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name, string favoriteFood) : base(name, favoriteFood)
+        {
+
+        }
+
+        public override string ExplainSelf()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(base.ExplainSelf())
+                .Append("MEEOW");
+
+            return builder.ToString();
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name, string favoriteFood) : base(name, favoriteFood)
+        {
+
+        }
+
+        public override string ExplainSelf()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(base.ExplainSelf())
+                .Append("DJAAF");
+
+            return builder.ToString();
+        }
+    }
+
+}
---
+++ 03_Programming_Advanced/04_C#_OOP/05_Polymorphism/PolymorphismLab/03_Shapes/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class Shape
+    {
+
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return "Drawing ";
+        }
+
+    }
+
+    public class Rectangle : Shape
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return this.radius; }
+            private set { this.radius = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+
+    }
+
+}
