6738ce00ddbc22277d2f9e10fecc8f24aa172071
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2020-03-11 15:25:03 +0200

    edit

---
+++ 03_Programming_Advanced/03_JS_Apps/03_PrototypesAndInheritance/Exercise/06_Computer.js
@@ -0,0 +1,103 @@
+function result() {
+
+  class Keyboard {
+    constructor(manufacturer,	responseTime) {
+      this.manufacturer = manufacturer;
+      this.responseTime = responseTime;
+    }
+  }
+
+  class Monitor {
+    constructor(manufacturer,	width, height) {
+      this.manufacturer = manufacturer;
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  class Battery {
+    constructor(manufacturer,	expectedLife) {
+      this.manufacturer = manufacturer;
+      this.expectedLife = expectedLife;
+    }
+  }
+
+  class Computer {
+    constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+      if (new.target === Computer) {
+        throw new TypeError("Abstract class cannot be instantiated directly");
+      }
+
+      this.manufacturer = manufacturer;
+      this.processorSpeed = processorSpeed;
+      this.ram = ram;
+      this.hardDiskSpace = hardDiskSpace;
+    }
+
+  }
+
+  class Laptop extends Computer {
+    constructor(manufacturer, processorSpeed, ram, hardDiskSpace, weight, color, battery){
+      super(manufacturer, processorSpeed, ram, hardDiskSpace);
+      this.weight = weight;
+      this.color = color;
+      this.battery = battery;
+    }
+
+    get battery() {
+      return this._battery;
+    }
+
+    set battery(newBattery) {
+      console.log(newBattery instanceof Battery);
+      console.log((newBattery instanceof Battery) == false);
+      debugger;
+      if(!(newBattery instanceof Battery)){
+        throw new TypeError("not a valid battery");
+      }
+      this._battery = newBattery;
+    }
+
+  }
+
+  class Desktop extends Computer {
+    constructor(manufacturer, processorSpeed, ram, hardDiskSpace, keyboard, monitor){
+        super(manufacturer, processorSpeed, ram, hardDiskSpace);
+        this.keyboard = keyboard;
+        this.monitor = monitor;
+    }
+
+    get keyboard(){
+        return this._keyboard;
+    }
+
+    set keyboard(newKeyboard){
+        if((newKeyboard instanceof Keyboard) == false){
+            throw new TypeError("Not a valid keyboard");
+        }
+        this._keyboard = newKeyboard;
+    }
+
+    get monitor(){
+        return this._monitor;
+    }
+
+    set monitor(newMonitor){
+        if((newMonitor instanceof Monitor) == false){
+            throw new TypeError("Not a valid monitor");
+        }
+        this._monitor = newMonitor;
+    }
+
+  }
+
+  return {
+    Battery: Battery,
+    Keyboard: Keyboard,
+    Monitor: Monitor,
+    Computer: Computer,
+    Laptop: Laptop,
+    Desktop: Desktop
+  }
+
+}
