2d8d25d371071562878e2431e790b67ff7f5c9a0
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2020-06-15 14:23:13 +0300

    edit

--- 04_Databases/MSSQL_Basics/04_BuiltInFunctions/BuiltInFunctions.sql
+++ 04_Databases/MSSQL_Basics/04_BuiltInFunctions/BuiltInFunctions.sql
@@ -125,3 +125,80 @@ SELECT
 FROM Peaks AS p, Rivers AS r 
 WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
 ORDER BY [Mix]
+
+-- Problem 14. Games from 2011 and 2012 year
+SELECT TOP 50 
+	g.[Name],
+	FORMAT(g.[Start], 'yyyy-MM-dd') AS [Start]
+FROM Games AS g
+WHERE YEAR(g.Start) IN (2011, 2012)
+ORDER BY 
+	[Start] ASC,
+    g.Name ASC;
+
+-- Problem 15. User Email Providers
+SELECT 
+	u.[Username],
+	SUBSTRING(u.[Email], CHARINDEX('@', u.[Email], 1) + 1, LEN(u.[Email])) AS [Email Provider]
+FROM Users AS u
+ORDER BY
+	[Email Provider] ASC,
+	u.[Username] ASC
+
+-- Problem 16. Get Users with IPAdress Like Pattern
+SELECT 
+	u.Username,
+	u.IpAddress
+FROM Users AS u
+WHERE u.IpAddress LIKE '___.1%.%.___'
+ORDER BY u.Username
+
+-- Problem 17. Show All Games with Duration and Part of the Day
+SELECT 
+	g.[Name] AS [Game],
+	CASE
+		WHEN DATEPART(HOUR, g.Start) >= 0 AND DATEPART(HOUR, g.Start) < 12 THEN 'Morning'
+        WHEN DATEPART(HOUR, g.Start) >= 12 AND DATEPART(HOUR, g.Start) < 18 THEN 'Afternoon'
+        WHEN DATEPART(HOUR, g.Start) >= 18 AND DATEPART(HOUR, g.Start) < 24 THEN 'Evening'
+    END AS [Part of the Day],
+	CASE
+		WHEN g.[Duration] <= 3 THEN 'Extra Short'
+		WHEN g.[Duration] >= 4 AND g.[Duration] <= 6 THEN 'Short'
+		WHEN g.[Duration] >= 6 THEN 'Long'
+		ELSE 'Extra Long'
+	END AS [Duration]
+FROM Games AS g
+ORDER BY
+	[Game] ASC,
+	[Duration] ASC;
+
+-- Problem 18. Orders Table
+SELECT 
+	o.ProductName,
+	o.OrderDate,
+	DATEADD(DAY, 3, o.OrderDate) AS [Pay Due],
+	DATEADD(MONTH, 1, o.OrderDate) AS [Deliver Due]
+FROM Orders AS o
+
+-- Problem 19. People Table
+CREATE TABLE People (
+    Id INT IDENTITY(1, 1) NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    Birthdate DATETIME NOT NULL,
+
+    CONSTRAINT PK_People
+    PRIMARY KEY (Id)
+)
+
+INSERT INTO People (Name, Birthdate) 
+     VALUES ('Victor', '2000-12-07'),
+            ('Steven', '1992-09-10'),
+            ('Stephen', '1910-09-19'),
+            ('John', '2010-01-06')
+
+SELECT p.Name,
+       DATEDIFF(YEAR, p.Birthdate, GETDATE()) AS [Age in Years],
+       DATEDIFF(MONTH, p.Birthdate, GETDATE()) AS [Age in Months],
+       DATEDIFF(DAY, p.Birthdate, GETDATE()) AS [Age in Days],
+       DATEDIFF(MINUTE, p.Birthdate, GETDATE()) AS [Age in Minutes]
+  FROM People AS p
