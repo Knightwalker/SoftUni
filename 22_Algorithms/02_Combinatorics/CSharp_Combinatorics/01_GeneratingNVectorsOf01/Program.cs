9e14df00761ebd14466f3c94900b93b211136edb
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2021-04-22 01:00:21 +0300

    refresh algorithms

---
+++ 22_Algorithms/02_Combinatorics/CSharp_Combinatorics/01_GeneratingNVectorsOf01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01_GeneratingNVectorsOf01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 3;
+            int[] vector = new int[n];
+            Gen01(vector, 0);
+        }
+
+        public static void Gen01(int[] vector, int index)
+        {
+            if (index >= vector.Length)
+            {
+                PrintVector(vector);
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                vector[index] = i;
+                Gen01(vector, index + 1);
+            }
+
+        }
+
+        public static void PrintVector(int[] vector) 
+        {
+            Console.WriteLine($"({string.Join(", ", vector)})");
+        }
+    
+    }
+}
---
+++ 22_Algorithms/02_Combinatorics/CSharp_Combinatorics/02_GeneratingNVectorsOfK/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01_GeneratingNVectorsOfK
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 3;
+            int startK = 0;
+            int endK = 3;
+            int[] vector = new int[n];
+            Gen01(vector, startK, endK, 0);
+        }
+
+        public static void Gen01(int[] vector, int startK, int endK, int index)
+        {
+            if (index >= vector.Length)
+            {
+                PrintVector(vector);
+                return;
+            }
+
+            for (int i = startK; i <= endK; i++)
+            {
+                vector[index] = i;
+                Gen01(vector, startK, endK, index + 1);
+            }
+
+        }
+
+        public static void PrintVector(int[] vector)
+        {
+            Console.WriteLine($"({string.Join(", ", vector)})");
+        }
+
+    }
+}
